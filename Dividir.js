0ed0d163a54604611d3c4e5caaa0d61a4822e756
Yael0702 <118012242+Yael0702@users.noreply.github.com>
2023-01-03 17:55:51 -0600

    jorrge.cruz@gmail.com

---
+++ Dividir.js
@@ -0,0 +1,15 @@
+//Ejercicio de division de dos numeros enteros
+let numero, divisor, constane, resto;
+numero - prompt("Dame un numero que quieras dividir:");
+divisor - prompt("Dame el divisor:");
+
+if(cosiente - Math.floor(cosiente) == 0){
+    console.log("es exacta");
+    console.log("Resultado:", cosiente);
+
+} else{
+    console.log("Es inexacta");
+    console.log("Resultado:", Math.floor(cosiente));
+    resto = cosiente - Math.floor(cosiente)
+    console.log("Resto:",resto);
+}        
